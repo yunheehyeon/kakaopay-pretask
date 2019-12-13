d440ac6bb05697e332725c2f2c7895bbc80aa72f
yunheehyeon <ok94life@daum.net>
2019-12-14 01:23:52 +0900

    feat: 지역명 커스텀 id로 저장

---
+++ src/main/java/kakaopay/problem/aipservice/domain/region/RegionRepository.java
@@ -0,0 +1,6 @@
+package kakaopay.problem.aipservice.domain.region;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, String> {
+}
---
+++ src/test/java/kakaopay/problem/aipservice/domain/region/RegionRepositoryTest.java
@@ -0,0 +1,31 @@
+package kakaopay.problem.aipservice.domain.region;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class RegionRepositoryTest {
+    @Autowired
+    private RegionRepository regionRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    @DisplayName("Region 커스텀키로 생성")
+    void save() {
+        Region region = Region.builder().name("성남").build();
+        Region persistRegion = testEntityManager.persist(region);
+        testEntityManager.flush();
+        testEntityManager.clear();
+
+        Region repoRegion = regionRepository.findById(persistRegion.getId()).get();
+
+        assertThat(repoRegion.getId()).contains("reg");
+    }
+}
